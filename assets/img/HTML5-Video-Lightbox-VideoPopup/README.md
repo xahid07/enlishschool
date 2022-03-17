5108987527b7185f9465b0376453ad60571cd7de
Md. Jahidul Islam <jahiduap111@gamil.com>
2022-03-17 21:10:58 +0600

    pull

--- assets/img/HTML5-Video-Lightbox-VideoPopup/README.md
+++
@@ -1,4 +0,0 @@
-# Video Modal - (html5 - Jquery.3.1.1) 
-	Simple, useful jquery modal
-
-
--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,51 +1,159 @@
-// BACK TO TOP BUTTON
-var mybutton = document.getElementById("up_btn");
+$ = jQuery.noConflict();
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-    scrollFunction();
-};
+$(document).ready(function () {
 
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        mybutton.style.opacity = "1";
-    } else {
-        mybutton.style.opacity = "0";
-    }
-}
-mybutton.addEventListener("click", topFunction);
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
+    let slider_items = $('.s_item');
+    let s_indicators = $('.indicator div');
+    let i=0;
+    //slider();
+    // function slider() {
+        // for (let i = 0; i < 3; i++) {
+        //     //$(slider_items[i+1]).fadeOut(200);
 
 
-$(function () {
+        // }
+        // let intervalID;
+        // function slider() {
+        //     if (i === 0) {
+        //         $(slider_items[2]).removeClass('active');
+        //         $(s_indicators[2]).removeClass('active');
+        //         i++
+        //         $(slider_items[0]).addClass('active');
+        //         $(s_indicators[0]).addClass('active');
+        //     }
+        //     else if (i === 1) {
+        //         $(slider_items[0]).removeClass('active');
+        //         $(s_indicators[0]).removeClass('active');
+        //         i++
+        //         $(slider_items[1]).addClass('active');
+        //         $(s_indicators[1]).addClass('active');
+        //     }
+        //     else if (i === 2) {
+        //         i++
+        //         $(slider_items[1]).removeClass('active');
+        //         $(s_indicators[1]).removeClass('active');
+        //         i = 0;
+        //         $(slider_items[2]).addClass('active');
+        //         $(s_indicators[2]).addClass('active');
+        //     }
 
-    //SIDE MENU IN MOBILE
-    $('.mobile-bars').click(function () {
-        if ($('.mobile-bars').hasClass('cross')) {
-            $('.main_menu').css("right", "0px");
+        // }
+        // //i = 0;
+        // if (!intervalID) {
+        //     intervalID = setInterval(() => {
+        //         slider()
+        //     }, 2000);
+        // }
+    // }
+
+    //FACILITIES RUN SND STOP ON HOVER
+    // $(document).on('mouseover','.s_item',()=>{
+    //     clearInterval(intervalID,()=>{
+    //         intervalID=null;
+    //     })
+    // })
+    // $(document).on('mouseleave','.s_item',()=>{
+    //     intervalID = setInterval(() => {
+    //         slider()
+    //     }, 2000);
+    // })
+
+    //FACILITIES ANCOR CLICK FUNCTION
+    $(document).on('click','.ancor:eq(0)',()=>{
+        if($('.s_item.active').attr('data-item')!='s-1')
+        {
+            $('.s_item.active').removeClass('active').prev().addClass('active');
+            $('.indicator div.active').removeClass('active').prev().addClass('active');
+            
+        }
+        
+        else{
+            $('.indicator div.active').removeClass('active');
+            $('.s_item.active').removeClass('active');
+            $('.s_item:eq(2)').addClass('active');
+            $('.indicator div:eq(2)').addClass('active');
+        }
+        
+    })
+    $(document).on('click','.ancor:eq(1)',()=>{
+        if($('.s_item.active').attr('data-item')!='s-3')
+        {
+            $('.s_item.active').removeClass('active').next().addClass('active');
+            $('.indicator div.active').removeClass('active').next().addClass('active');
+        }
+        
+        else{
+            $('.indicator div.active').removeClass('active');
+            $('.s_item.active').removeClass('active');
+            $('.s_item:eq(0)').addClass('active');
+            $('.indicator div:eq(0)').addClass('active');
+        }
+        
+    })
+
+    $(document).on('click','.menu_bars',()=>{
+        //let widt=$('.m_menu').width()
+        
+        //($($('.m_menu')).height()===400)?$($('.m_menu')).animate({height:0},300):$($('.m_menu')).animate({height:400},300)
+        if($($('.m_menu')).width()===270){
+            $('.menu_bars').css('padding-left','2px')
+            $('.bar:first-child + .bar').css('visibility','visible');
+            $('.bar:first-child').css('transform','rotate(0deg) translate(0px,0px)');
+            $('.bar:last-child').css('transform','rotate(0deg) translate(0px,0px)');
+            $($('.m_menu')).animate({width:0},300)
         }
         else{
-            $('.main_menu').css("right", "-100%");
-        }
-    });
-
-    // STICKY HEADER
-    $(window).on("scroll", function () {
-        if ($(window).scrollTop()) {
-            $(".navigation").addClass('sticky-nav')
-        }
-        else {
-            $(".navigation").removeClass('sticky-nav');
+            $('.menu_bars').css('padding-left','5px')
+            $('.bar:first-child + .bar').css('visibility','hidden');
+            $('.bar:first-child').css('transform','rotate(45deg) translate(6px,10px)');
+            $('.bar:last-child').css('transform','rotate(-45deg) translate(7px,-10px)');
+            $($('.m_menu')).animate({width:270},300)
         }
     })
 
-    //Simple Lightbox Enable
-    $('.gallery a').simpleLightbox();
+    //NOTICE BOARD STOP AND RUN ON HOVER
+    // $(document).on('mouseover','.notices',()=>{
+    //     clearInterval(intervalID,()=>{
+    //         intervalID=null;
+    //     })
+    // })
+    // $(document).on('mouseleave','.notices',()=>{
+    //     intervalID = setInterval(() => {
+    //         slider()
+    //     }, 2000);
+    // })
+
+
+    //PAYPAL INTEGRATION
+    paypal.Buttons({
+
+        // Set up the transaction
+        createOrder: function(data, actions) {
+            return actions.order.create({
+                purchase_units: [{
+                    amount: {
+                        value: '88.44'
+                    }
+                }]
+            });
+        },
+
+        // Finalize the transaction
+        onApprove: function(data, actions) {
+            return actions.order.capture().then(function(orderData) {
+                // Successful capture! For demo purposes:
+                console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
+                var transaction = orderData.purchase_units[0].payments.captures[0];
+                alert('Transaction '+ transaction.status + ': ' + transaction.id + '\n\nSee console for all available details');
+
+                // Replace the above to show a success message within this page, e.g.
+                // const element = document.getElementById('paypal-button-container');
+                // element.innerHTML = '';
+                // element.innerHTML = '<h3>Thank you for your payment!</h3>';
+                // Or go to another URL:  actions.redirect('thank_you.html');
+            });
+        }
+
+
+    }).render('#paypal-button-container');
 })
-
-
-
